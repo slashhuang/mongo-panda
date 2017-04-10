8ffa8cf9e8017d2c3e8a3f88d013267234dde0be
slashhuang <627284482@qq.com>
2017-04-10 23:14:36 +0800

    add index

--- README.md
+++ README.md
@@ -5,11 +5,21 @@ a mongodb client reference handbook
 
 ## links
 
+mongo及shell概述
 
 [start](./doc/start.md)
 
+mongodb实例方法
+
 [db](./doc/db.md)
 
+
+mongodb的collection实例方法
+
 [collection](./doc/collection.md)
+
+## 官方 links
+
+[index索引优化](https://docs.mongodb.com/manual/tutorial/analyze-query-plan/)
 
 [CRUD](http://mongodb.github.io/node-mongodb-native/2.2/tutorials/crud/)
--- example/index.js
+++ example/index.js
@@ -6,9 +6,11 @@ var url = 'mongodb://localhost:27017/fuck';
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
-  insertDocuments(db,()=>{
-        db.close
-  	})
+  insertDocuments(db, function() {
+    indexCollection(db, function() {
+      db.close();
+    });
+  });
 });
 
 //插入文档
@@ -18,11 +20,22 @@ var insertDocuments = function(db, callback) {
   var collection = db.collection('hekkk');
   //由于mongo以_id为主键，因此不存在增量覆盖一说
   collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
+    {a : 1}, {a : 2}, {a : 3},{b:2}
   ], function(err, result) {
   console.log(result)
     console.log("Inserted 3 hekkk into the collection");
     callback(result);
   });
 }
+//索引文件
+var indexCollection = function(db, callback) {
+  db.collection('hekkk').createIndex(
+    { "b": -1 },
+      null,
+      function(err, results) {
+        console.log('---',results);
+        callback();
+    }
+  );
+};
 
